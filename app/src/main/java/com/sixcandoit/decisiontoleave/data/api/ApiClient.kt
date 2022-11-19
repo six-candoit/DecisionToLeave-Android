85806fee87bce10b817676756787750870371b74
SunWoong <sunwoong1122@gmail.com>
2022-11-20 01:51:01 +0900

    [feat] api client 구현

---
+++ app/src/main/java/com/sixcandoit/decisiontoleave/data/api/ApiClient.kt
@@ -0,0 +1,32 @@
+package com.sixcandoit.decisiontoleave.data.api
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object ApiClient {
+    private const val BASE_URL = "http://43.200.42.222/"
+    private var retrofit: Retrofit? = null
+
+    @OptIn(ExperimentalSerializationApi::class)
+    fun getRetrofit(): Retrofit {
+        if (retrofit == null) {
+            val logger = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+                .build()
+        }
+        return retrofit!!
+    }
+}
